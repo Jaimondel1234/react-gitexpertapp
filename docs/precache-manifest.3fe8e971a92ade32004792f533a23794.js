a40cf85af88aeac2d2793aafb38be991b2b1fe16
Jaime <jaimondel1234@gmail.com>
2020-09-10 13:29:53 +0100

    docs agregado

---
+++ docs/precache-manifest.3fe8e971a92ade32004792f533a23794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813b30681afcb826df923ea2f4b19d2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d75b392d65b1bed10819",
+    "url": "/static/css/main.a2d70d25.chunk.css"
+  },
+  {
+    "revision": "7df93c7276edf1b25086",
+    "url": "/static/js/2.65d56cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65d56cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75b392d65b1bed10819",
+    "url": "/static/js/main.654c36d0.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
