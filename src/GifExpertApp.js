042e01fbb6ab1aa7159a5a12abdffeab0d6ba624
Jaime <jaimondel1234@gmail.com>
2020-09-10 13:28:44 +0100

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  /*   const handleAdd = () => {
+    setCategories([...categories, "Hunter x Hunter"]);
+    //setCategories((cats) => [...cats, "Hunter x Hunter"]);
+  }; */
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+      {/* <button onClick={handleAdd}>Agregar</button> */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
